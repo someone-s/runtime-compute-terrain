ba3dacc912673e53e2dc60e4c2ffec4a910d770d
wonge1 <e.wong1@lancaster.ac.uk>
2025-04-14 19:41:25 +0100

    projection working

--- Assets/Terrains/Scripts/TerrainModifier.cs
+++ Assets/Terrains/Scripts/TerrainModifier.cs
@@ -7,15 +7,16 @@ using Unity.Mathematics;
 using System.IO;
 using System.Linq;
 using UnityEngine.InputSystem;
+using System;
 
 [RequireComponent(typeof(TerrainCoordinator))]
 public class TerrainModifier : MonoBehaviour
 {
 
     // General state
-    private static float area => TerrainCoordinator.area;
-    private static int meshSize => TerrainCoordinator.meshSize;
-    [SerializeField] private ComputeShader computeShader;
+    private const float area = TerrainCoordinator.area;
+    private const int meshSize = TerrainCoordinator.meshSize;
+    private const int quadSize = meshSize * 2 + 1;
     private TerrainCoordinator coordinator;
 
     #region Configuration Region
@@ -24,12 +25,13 @@ public class TerrainModifier : MonoBehaviour
     {
         coordinator = GetComponent<TerrainCoordinator>();
 
-        computeShader = Instantiate(computeShader);
+        modifyShader = Instantiate(modifyShader);
 
 
-        computeShader.SetFloat(Shader.PropertyToID("_Area"), area);
-        computeShader.SetInt(Shader.PropertyToID("_Size"), meshSize);
-        computeShader.SetInt(Shader.PropertyToID("_MeshSection"), Mathf.CeilToInt(((float)(meshSize * 2 + 1)) / 32f));
+        modifyShader.SetFloat("_Area", area);
+        modifyShader.SetInt("_Size", meshSize);
+        modifyShader.SetInt("_QuadSize", quadSize);
+        modifyShader.SetInt("_MeshSection", Mathf.CeilToInt(quadSize / 32f));
 
         SetupModify();
 
@@ -39,33 +41,34 @@ public class TerrainModifier : MonoBehaviour
     private void OnDestroy()
     {
         operationBuffer.Dispose();
+        projectBuffer.Dispose();
     }
 
     private TerrainController[] SetArea(int kernelIndex, (int x, int z) region)
     {
 
-        computeShader.SetInt(Shader.PropertyToID("_Stride"), TerrainController.VertexBufferStride);
-        computeShader.SetInt(Shader.PropertyToID("_PositionOffset"), TerrainController.VertexPositionAttributeOffset);
-        computeShader.SetInt(Shader.PropertyToID("_NormalOffset"), TerrainController.VertexNormalAttributeOffset);
-        computeShader.SetInt(Shader.PropertyToID("_BaseOffset"), TerrainController.VertexBaseAttributeOffset);
-        computeShader.SetInt(Shader.PropertyToID("_ModifyOffset"), TerrainController.VertexModifyAttributeOffset);
+        modifyShader.SetInt("_Stride", TerrainController.VertexBufferStride);
+        modifyShader.SetInt("_PositionOffset", TerrainController.VertexPositionAttributeOffset);
+        modifyShader.SetInt("_NormalOffset", TerrainController.VertexNormalAttributeOffset);
+        modifyShader.SetInt("_BaseOffset", TerrainController.VertexBaseAttributeOffset);
+        modifyShader.SetInt("_ModifyOffset", TerrainController.VertexModifyAttributeOffset);
 
         TerrainController[] controllers = new TerrainController[4];
         
         TerrainController controllerBL = coordinator.controllers[region];
-        computeShader.SetBuffer(kernelIndex, Shader.PropertyToID("_VerticesBL"), controllerBL.vertexBuffer);
+        modifyShader.SetBuffer(kernelIndex, "_VerticesBL", controllerBL.vertexBuffer);
         controllers[0] = controllerBL;
 
         TerrainController controllerBR = coordinator.controllers[(region.x + 1, region.z)];
-        computeShader.SetBuffer(kernelIndex, Shader.PropertyToID("_VerticesBR"), controllerBR.vertexBuffer);
+        modifyShader.SetBuffer(kernelIndex, "_VerticesBR", controllerBR.vertexBuffer);
         controllers[1] = controllerBR;
 
         TerrainController controllerTL = coordinator.controllers[(region.x, region.z + 1)];
-        computeShader.SetBuffer(kernelIndex, Shader.PropertyToID("_VerticesTL"), controllerTL.vertexBuffer);
+        modifyShader.SetBuffer(kernelIndex, "_VerticesTL", controllerTL.vertexBuffer);
         controllers[2] = controllerTL;
 
         TerrainController controllerTR = coordinator.controllers[(region.x + 1, region.z + 1)];
-        computeShader.SetBuffer(kernelIndex, Shader.PropertyToID("_VerticesTR"), controllerTR.vertexBuffer);
+        modifyShader.SetBuffer(kernelIndex, "_VerticesTR", controllerTR.vertexBuffer);
         controllers[3] = controllerTR;
 
         return controllers;
@@ -80,6 +83,9 @@ public class TerrainModifier : MonoBehaviour
     
     [Header("Modify Section")]
     [SerializeField] private int modifyBufferCount = 50;
+    [SerializeField] private ComputeShader modifyShader;
+    private int modifyMeshKernel;
+    private int modifyApplyKernel;
 
     private Queue<((int x, int z) region, Operation operation)> operationQueue = new Queue<((int x, int z) region, Operation operation)>();
 
@@ -102,10 +108,10 @@ public class TerrainModifier : MonoBehaviour
 
     private void SetupModify()
     {
-        int modifyMeshKernelIndex = computeShader.FindKernel("ModifyMesh");
+        modifyMeshKernel = modifyShader.FindKernel("ModifyMesh");
         
         operationBuffer = new ComputeBuffer(modifyBufferCount, sizeof(float) * 4 + sizeof(uint), ComputeBufferType.Structured, ComputeBufferMode.SubUpdates);
-        computeShader.SetBuffer(modifyMeshKernelIndex, Shader.PropertyToID("_Operations"), operationBuffer);
+        modifyShader.SetBuffer(modifyMeshKernel, "_Operations", operationBuffer);
     }
 
     public void QueueModify((int x, int y) region, Vector2 normalPosition, float normalRadius, float operationParameter, OperationType operationType)
@@ -149,17 +155,15 @@ public class TerrainModifier : MonoBehaviour
 
         operationBuffer.EndWrite<Operation>(index);
 
-        computeShader.SetInt(Shader.PropertyToID("_Count"), index);
-
-        int modifyMeshKernelIndex = computeShader.FindKernel("ModifyMesh");
+        modifyShader.SetInt("_Count", index);
 
         if (targetRegion != currentModifyRegion)
         {
             currentModifyRegion = targetRegion;
-            currentModifyControllers = SetArea(modifyMeshKernelIndex, currentModifyRegion.Value);
+            currentModifyControllers = SetArea(modifyMeshKernel, currentModifyRegion.Value);
         }
 
-        computeShader.Dispatch(modifyMeshKernelIndex, 1, 1, 1);
+        modifyShader.Dispatch(modifyMeshKernel, 1, 1, 1);
 
         foreach (TerrainController controller in currentModifyControllers)
             controller.OnTerrainChange.Invoke();
@@ -170,23 +174,18 @@ public class TerrainModifier : MonoBehaviour
     #region Project Section
 
     [Header("Project Section")]
-    [SerializeField] private float depth = 1000f;
-    private float start => depth * 0.5f;
-    private float ignore => 1f / depth;
-
-
-    [SerializeField] private Transform cameraRig;
-    [SerializeField] private Camera mandateCamera;
-    private RenderTexture mandateTexture;
-    [SerializeField] private Camera minimumCamera;
-    private RenderTexture minimumTexture;
-    [SerializeField] private Camera maximumCamera;
-    private RenderTexture maximumTexture;
-
     [SerializeField] private ComputeShader projectShader;
-    [SerializeField] private List<MeshFilter> filters;
+    private int projectExecuteKernel;
+    private int projectClearKernel;
+    private int projectConvertKernel;
     [SerializeField] private GraphicsBuffer projectBuffer;
-    [SerializeField] private Shader shader;
+    [SerializeField] private List<MeshFilter> filters;
+
+    private Matrix4x4 projectionMatrix;
+    private Quaternion projectionRotation;
+    private Vector3 projectionScale;
+    private Vector3 projectionPosition;
+    private float projectionHalfPitch;
 
     [SerializeField] private int projectBatchSize = 1;
 
@@ -194,57 +193,32 @@ public class TerrainModifier : MonoBehaviour
 
     private void SetupProject() 
     {
-        RenderTextureDescriptor descriptor = new RenderTextureDescriptor(
-            width: meshSize * 2 + 1,
-            height: meshSize * 2 + 1,
-            colorFormat: UnityEngine.Experimental.Rendering.GraphicsFormat.None,
-            depthStencilFormat: UnityEngine.Experimental.Rendering.GraphicsFormat.D32_SFloat
-        );
-
-        mandateTexture = new RenderTexture(descriptor);
-        minimumTexture = new RenderTexture(descriptor);
-        maximumTexture = new RenderTexture(descriptor);
-
-        int applyModifiersKernelIndex = computeShader.FindKernel("ApplyModifiers");
-
-        computeShader.SetTexture(applyModifiersKernelIndex, Shader.PropertyToID("_MandateRT"), mandateTexture);
-        computeShader.SetTexture(applyModifiersKernelIndex, Shader.PropertyToID("_MinimumRT"), minimumTexture);
-        computeShader.SetTexture(applyModifiersKernelIndex, Shader.PropertyToID("_MaximumRT"), maximumTexture);
-
-        computeShader.SetFloat(Shader.PropertyToID("_Start"), start);
-        computeShader.SetFloat(Shader.PropertyToID("_Depth"), depth);
-        computeShader.SetFloat(Shader.PropertyToID("_Ignore"), ignore);
-
-        mandateCamera.orthographicSize = area + (area / meshSize * 0.5f);
-        mandateCamera.nearClipPlane = 0f;
-        mandateCamera.farClipPlane = depth;
-        mandateCamera.transform.localPosition = new Vector3(0f, start, 0f);
-        mandateCamera.transform.localRotation = Quaternion.Euler(90f, 0f, 0f);
-        mandateCamera.targetTexture = mandateTexture;
-        mandateCamera.SetReplacementShader(shader, string.Empty);
         
-        minimumCamera.orthographicSize = area + (area / meshSize * 0.5f);
-        minimumCamera.nearClipPlane = 0f;
-        minimumCamera.farClipPlane = depth;
-        minimumCamera.transform.localPosition = new Vector3(0f, start, 0f);
-        minimumCamera.transform.localRotation = Quaternion.Euler(90f, 0f, 0f);
-        minimumCamera.targetTexture = minimumTexture;
-        minimumCamera.SetReplacementShader(shader, string.Empty);
+        projectBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Raw, quadSize * quadSize, sizeof(float) * 3);
 
-        maximumCamera.orthographicSize = area + (area / meshSize * 0.5f);
-        maximumCamera.nearClipPlane = 0f;
-        maximumCamera.farClipPlane = depth;
-        maximumCamera.transform.localPosition = new Vector3(0f, -start, 0f);
-        maximumCamera.transform.localRotation = Quaternion.Euler(270f, 0f, 0f);
-        maximumCamera.targetTexture = maximumTexture;
-        maximumCamera.SetReplacementShader(shader, string.Empty);
+        modifyApplyKernel = modifyShader.FindKernel("ApplyModifiers");
 
-        projectBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Raw, (meshSize * 2 + 1) * (meshSize * 2 + 1), sizeof(float) * 3);
-        int kernel = projectShader.FindKernel("Project");
-        projectShader.SetInts("_Dimensions", (meshSize * 2 + 1), (meshSize * 2 + 1));
-        projectShader.SetBuffer(kernel, "_Depth", projectBuffer);
-        projectShader.SetBuffer(projectShader.FindKernel("Clear"), "_Depth", projectBuffer);
-        projectShader.SetBuffer(projectShader.FindKernel("Convert"), "_Depth", projectBuffer);
+        modifyShader.SetBuffer(modifyApplyKernel, "_Depth", projectBuffer);
+        modifyShader.SetFloat("_InvalidMin", -1000000000);
+        modifyShader.SetFloat("_InvalidMax",  1000000000);
+        
+        projectExecuteKernel = projectShader.FindKernel("Project");
+        projectClearKernel = projectShader.FindKernel("Clear");
+        projectConvertKernel = projectShader.FindKernel("Convert");
+
+        projectShader.SetFloat("_InvalidMin", -1000000000);
+        projectShader.SetFloat("_InvalidMax",  1000000000);
+        projectShader.SetInts("_Dimensions", quadSize, quadSize);
+        
+        projectShader.SetBuffer(projectExecuteKernel, "_Depth", projectBuffer);
+        projectShader.SetBuffer(projectClearKernel, "_Depth", projectBuffer);
+        projectShader.SetBuffer(projectConvertKernel, "_Depth", projectBuffer);
+
+        projectionMatrix = Matrix4x4.Ortho(-1f, 1f, -1f, 1f, -1f, 1f);
+        projectionHalfPitch = area / meshSize * 0.5f;
+        projectionPosition = new Vector3(0f, 0f, 0f);
+        projectionRotation = Quaternion.LookRotation(Vector3.down, Vector3.forward);
+        projectionScale = new Vector3((area + projectionHalfPitch) * 2f, (area + projectionHalfPitch) * 2f, 1f);
     }
 
     public void QueueProject((int x, int z) region)
@@ -258,7 +232,7 @@ public class TerrainModifier : MonoBehaviour
     private void T(string s)
     {
 
-            float[] output = new float[(meshSize * 2 + 1) * (meshSize * 2 + 1) * 3];
+            float[] output = new float[quadSize * quadSize * 3];
             projectBuffer.GetData(output);
             string path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments) + "/" + s;
             Debug.Log(path);
@@ -272,39 +246,36 @@ public class TerrainModifier : MonoBehaviour
 
             (int x, int z) = region;
 
-            cameraRig.localPosition = new Vector3((x + 1) * area, 0f, (z + 1) * area);
-            //mandateCamera.Render();
-            //minimumCamera.Render();
-            //maximumCamera.Render();
-            Vector3 center = new Vector3((x + 1) * area, 50f, (z + 1) * area);
-            float span = area + (area / meshSize * 0.5f);
+            projectShader.Dispatch(projectClearKernel, Mathf.CeilToInt((float)quadSize / 32), Mathf.CeilToInt((float)quadSize / 32), 1);
 
-            projectShader.Dispatch(projectShader.FindKernel("Clear"), Mathf.CeilToInt((float)(meshSize * 2 + 1) / 32), Mathf.CeilToInt((float)(meshSize * 2 + 1) / 32), 1);
+            projectionPosition.x = x * area + projectionHalfPitch;
+            projectionPosition.z = z * area + projectionHalfPitch;
+            projectShader.SetMatrix("_WorldToClip", projectionMatrix * Matrix4x4.Inverse(Matrix4x4.TRS(projectionPosition, projectionRotation, projectionScale)));
 
-            int kernel = projectShader.FindKernel("Project");
-            projectShader.SetMatrix("_WorldToClip", Matrix4x4.Ortho(-span, span, -span, span, 0, 100) * Matrix4x4.Inverse(Matrix4x4.TRS(center, Quaternion.Euler(90f, 0f, 0f), new Vector3(1,1,-1))));
-            //projectShader.SetMatrix("_WorldToClip", mandateCamera.projectionMatrix * mandateCamera.worldToCameraMatrix);
+            projectShader.SetInt("_Mode", 2);
             foreach (var filter in filters)
             {
                 projectShader.SetMatrix("_LocalToWorld", filter.transform.localToWorldMatrix);
-                projectShader.SetBuffer(kernel, "_Vertices", filter.sharedMesh.GetVertexBuffer(0));
-                projectShader.SetBuffer(kernel, "_Indices", filter.sharedMesh.GetIndexBuffer());
-                projectShader.SetInt("_Stride", filter.sharedMesh.GetVertexBufferStride(0));
-                projectShader.SetInt("_PositionOffset", filter.sharedMesh.GetVertexAttributeOffset(VertexAttribute.Position));
+
+                projectShader.SetBuffer(projectExecuteKernel, "_Vertices",       filter.sharedMesh.GetVertexBuffer(0));
+                projectShader.SetInt(                         "_Stride",         filter.sharedMesh.GetVertexBufferStride(0));
+                projectShader.SetInt(                         "_PositionOffset", filter.sharedMesh.GetVertexAttributeOffset(VertexAttribute.Position));
+
                 int triangleCount = (int)filter.sharedMesh.GetIndexCount(0) / 3;
-                projectShader.SetInt("_NumTriangle", triangleCount);
-                projectShader.Dispatch(kernel, Mathf.CeilToInt(triangleCount / 512), 1, 1);
+                projectShader.SetBuffer(projectExecuteKernel, "_Indices",     filter.sharedMesh.GetIndexBuffer());
+                projectShader.SetInt(                         "_NumTriangle", triangleCount);
+
+                projectShader.Dispatch(projectExecuteKernel, Mathf.CeilToInt(triangleCount / 512), 1, 1);
             }
-            projectShader.Dispatch(projectShader.FindKernel("Convert"), Mathf.CeilToInt((float)(meshSize * 2 + 1) / 32), Mathf.CeilToInt((float)(meshSize * 2 + 1) / 32), 1);
+            
+            projectShader.Dispatch(projectConvertKernel, Mathf.CeilToInt((float)quadSize / 32), Mathf.CeilToInt((float)quadSize / 32), 1);
 
-            if (Keyboard.current.xKey.isPressed)
-                T("temp.txt");
+            if (Keyboard.current.xKey.isPressed && x == -1 && z == -1)
+                T($"temp{x}-{z}.txt");
 
-            int applyModifiersKernelIndex = computeShader.FindKernel("ApplyModifiers");
+            TerrainController[] currentProjectController = SetArea(modifyApplyKernel, region);
 
-            TerrainController[] currentProjectController = SetArea(applyModifiersKernelIndex, region);
-
-            computeShader.Dispatch(applyModifiersKernelIndex, 1, 1, 1);
+            modifyShader.Dispatch(modifyApplyKernel, 1, 1, 1);
 
             foreach (TerrainController controller in currentProjectController)
                 controller.OnTerrainChange.Invoke();
