026ccdde07e2fd64bc851b7f2361ae69c4f33182
wonge1 <e.wong1@lancaster.ac.uk>
2025-04-15 23:46:36 +0100

    optimize spline generation complete

--- Assets/Tracks/Scripts/TrackController.cs
+++ Assets/Tracks/Scripts/TrackController.cs
@@ -1,41 +1,43 @@
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
+using System.IO;
+using System.Linq;
 using Unity.Collections;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
-using UnityEngine.Splines;
 
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
 public class TrackController : MonoBehaviour
 {
     [SerializeField] private ComputeShader computeShader;
-    private Spline spline;
+    private int updateTrackKernel;
+    private uint threadCount;
+    
+    private Vector3 p0, p1, p2, p3;
 
     #region Knot Section
     public void SetPoints(Vector3 aPos, Quaternion aRot, Vector3 bPos, Quaternion bRot)
     {
-        BezierKnot[] knots = new BezierKnot[2];
-        knots[0].Position = aPos - transform.position;
-        knots[0].TangentOut = aRot * Vector3.forward * Vector3.Distance(aPos, bPos) * 0.5f;
-        knots[1].Position = bPos - transform.position;
-        knots[1].TangentIn = bRot * Vector3.forward * Vector3.Distance(aPos, bPos) * 0.5f;
-        spline.Knots = knots;
+        p0 = aPos - transform.position;
+        p1 = p0 + aRot * Vector3.forward * Vector3.Distance(aPos, bPos) * 0.5f;
+        p3 = bPos - transform.position;
+        p2 = p3 + bRot * Vector3.forward * Vector3.Distance(aPos, bPos) * 0.5f;
+
     }
     #endregion
     
-    private static int maxSliceCount = 512;
+    private static int maxPointCount = 512;
+    private static float targetSegmentLength = 1f;
 
     private TrackProfile profile;
     private Mesh mesh;
     public GraphicsBuffer graphicsBuffer { get; private set; }
-    private ComputeBuffer pointsBuffer;
+    
     public void Setup(TrackProfile profileToUse)
     {
         profile = profileToUse;
-
-        spline = new Spline(2);
 
         MeshFilter filter = GetComponent<MeshFilter>();
         mesh = MeshGenerator.GetMesh(profile);
@@ -46,100 +48,64 @@ public class TrackController : MonoBehaviour
 
         computeShader = Instantiate(computeShader);
 
-        int updateTrackKernelIndex = computeShader.FindKernel("UpdateTrack");
+        updateTrackKernel = computeShader.FindKernel("UpdateTrack");
 
-        computeShader.SetInt(Shader.PropertyToID("_SliceSize"), profile.count);
+        computeShader.SetInt("_MaxPoint", maxPointCount);
+        computeShader.SetInt("_SliceSize", profile.count);
 
-        computeShader.SetInt(Shader.PropertyToID("_Stride"), MeshGenerator.GetVertexBufferStride(profile));
-        computeShader.SetInt(Shader.PropertyToID("_PositionOffset"), MeshGenerator.GetVertexPositionAttributeOffset(profile));
-        computeShader.SetInt(Shader.PropertyToID("_NormalOffset"), MeshGenerator.GetVertexNormalAttributeOffset(profile));
+        computeShader.SetInt("_Stride", MeshGenerator.GetVertexBufferStride(profile));
+        computeShader.SetInt("_PositionOffset", MeshGenerator.GetVertexPositionAttributeOffset(profile));
+        computeShader.SetInt("_NormalOffset", MeshGenerator.GetVertexNormalAttributeOffset(profile));
 
-        computeShader.SetBuffer(updateTrackKernelIndex, Shader.PropertyToID("_SourceVertices"), MeshGenerator.GetReference(profile));
-        computeShader.SetBuffer(updateTrackKernelIndex, Shader.PropertyToID("_DestVertices"), graphicsBuffer);
-        
-        pointsBuffer = new ComputeBuffer(maxSliceCount, sizeof(float) * 7, ComputeBufferType.Structured, ComputeBufferMode.SubUpdates);
-        computeShader.SetBuffer(updateTrackKernelIndex, Shader.PropertyToID("_Points"), pointsBuffer);
+        computeShader.SetBuffer(updateTrackKernel, "_SourceVertices", MeshGenerator.GetReference(profile));
+        computeShader.SetBuffer(updateTrackKernel, "_DestVertices", graphicsBuffer);
 
+        computeShader.GetKernelThreadGroupSizes(updateTrackKernel, out threadCount, out _, out _);
     }
 
-    [StructLayout(LayoutKind.Sequential)]
-    private struct Point
+
+    public void QueueRefresh() 
     {
-        public Vector3 position;
-        public Quaternion rotation;
-    }
-
-    public void QueueRefresh() {
         enabled = true;
     }
-    private static float targetSegmentLength = 1f;
     private void ExecuteRefresh()
     {
-        float totalLength = CurveUtility.ApproximateLength(spline.GetCurve(0));
-        int segmentCount = Mathf.Min(Mathf.CeilToInt(totalLength / targetSegmentLength), maxSliceCount - 1);
-        float actualSegmentLength = totalLength / segmentCount;
+        float approxLength = (Vector3.Distance(p0, p1) + Vector3.Distance(p1, p2) + Vector3.Distance(p2, p3) + Vector3.Distance(p3, p0)) * 0.5f;
+        int segmentCount = Mathf.Min(Mathf.CeilToInt(approxLength / targetSegmentLength), maxPointCount - 1);
+        float actualSpacing = approxLength / segmentCount;
         int pointCount = segmentCount + 1;
 
-        Vector3 meshMin = Vector3.positiveInfinity;
-        Vector3 meshMax = Vector3.negativeInfinity;
+        computeShader.SetFloat("_ApproxLength", approxLength);
+        computeShader.SetFloat("_ActualSpacing", actualSpacing);
+        computeShader.SetInt("_PointCount", pointCount);
+        computeShader.SetInt("_PointPerThread", Mathf.CeilToInt((float)pointCount / threadCount));
 
-        NativeArray<Point> pointsDestination = pointsBuffer.BeginWrite<Point>(0, pointCount);
-        float t = 0;
-        for (int p = 0; p < pointCount; p++)
-        {
-            spline.Evaluate(t, out float3 position, out float3 tangent, out float3 upVector);
-            pointsDestination[p] = new Point
-            {
-                position = position,
-                rotation = profile.vertical ? quaternion.LookRotation(Vector3.Normalize(new Vector3(tangent.x, 0f, tangent.z)), Vector3.up) : quaternion.LookRotation(tangent, upVector)
-            };
-            // if (position.x < meshMin.x)
-            //     meshMin.x = position.x;
-            // if (position.y < meshMin.y)
-            //     meshMin.y = position.y;
-            // if (position.z < meshMin.z)
-            //     meshMin.z = position.z;
-            // if (position.x > meshMax.x)
-            //     meshMax.x = position.x;
-            // if (position.y > meshMax.y)
-            //     meshMax.y = position.y;
-            // if (position.z > meshMax.z)
-            //     meshMax.z = position.z;
-            spline.GetPointAtLinearDistance(t, actualSegmentLength, out t);
-        }
-        pointsBuffer.EndWrite<Point>(pointCount);
+        computeShader.SetVector("_P0", p0);
+        computeShader.SetVector("_P1", p1);
+        computeShader.SetVector("_P2", p2);
+        computeShader.SetVector("_P3", p3);
 
-        computeShader.SetInt(Shader.PropertyToID("_PointCount"), pointCount);
+        computeShader.SetVector("_US", Vector3.up);
+        computeShader.SetVector("_UE", Vector3.up);
 
-        computeShader.Dispatch(computeShader.FindKernel("UpdateTrack"), 1, 1, 1);
+        computeShader.Dispatch(updateTrackKernel, 1, 1, 1);
 
-        Bounds b = spline.GetBounds();
-        b.Expand(profile.extent);
-        mesh.bounds = b;
+        Bounds bounds = new Bounds(p0, Vector3.zero);
+        bounds.Encapsulate(p1);
+        bounds.Encapsulate(p2);
+        bounds.Encapsulate(p3);
+        bounds.Expand(profile.extent);
+        mesh.bounds = bounds;
 
         SubMeshDescriptor descriptor = new SubMeshDescriptor {
             baseVertex = 0,
             firstVertex = 0,
-            vertexCount = maxSliceCount * profile.count,
+            vertexCount = pointCount * profile.count,
             indexStart = 0,
-            indexCount = 6 * (maxSliceCount - 1) * (profile.count - 1),
+            indexCount = 6 * segmentCount * (profile.count - 1),
             topology = MeshTopology.Triangles,
-            bounds = b
+            bounds = bounds
         };
-
-        // int gotPoints = 50; // from compute shader
-        // Bounds bound = new Bounds(); // from compute shader
-        // int vertexCountPerPoint = 5; // from track profile
-
-        // SubMeshDescriptor descriptor = new SubMeshDescriptor {
-        //     baseVertex = 0,
-        //     firstVertex = 0,
-        //     vertexCount = gotPoints * vertexCountPerPoint,
-        //     indexStart = 0,
-        //     indexCount = 6 * (gotPoints - 1) * (vertexCountPerPoint - 1),
-        //     topology = MeshTopology.Triangles,
-        //     bounds = bound
-        // };
 
         MeshUpdateFlags updateFlags = 
             MeshUpdateFlags.DontValidateIndices |    // dont check against CPU index buffer
@@ -148,8 +114,6 @@ public class TrackController : MonoBehaviour
             MeshUpdateFlags.DontRecalculateBounds;   // dont recalculate bounds
 
         mesh.SetSubMesh(0, descriptor, updateFlags);
-
-        //GetComponent<MeshRenderer>().subMeshStartIndex =
     }
 
     public TerrainCoordinator coordinator;
@@ -219,25 +183,25 @@ public class TrackController : MonoBehaviour
 
         private static Entry CreateMesh(TrackProfile profile)
         {
-            NativeArray<float3> vertices = new NativeArray<float3>(profile.count * maxSliceCount, Allocator.Temp);
-            NativeArray<float3> normals = new NativeArray<float3>(profile.count * maxSliceCount, Allocator.Temp);
-            for (int s = 0; s < maxSliceCount; s++)
+            NativeArray<float3> vertices = new NativeArray<float3>(profile.count * maxPointCount, Allocator.Temp);
+            NativeArray<float3> normals = new NativeArray<float3>(profile.count * maxPointCount, Allocator.Temp);
+            for (int s = 0; s < maxPointCount; s++)
                 for (int p = 0; p < profile.count; p++)
                 {
                     vertices[s * profile.count + p] = profile.points[p];
                     normals[s * profile.count + p] = profile.normals[p];
                 }
 
-            NativeArray<int> indices = new NativeArray<int>(6 * (profile.count - 1) * (maxSliceCount - 1), Allocator.Temp);
-            for (int s = 0; s < (maxSliceCount - 1); s++)
+            NativeArray<int> indices = new NativeArray<int>(6 * (profile.count - 1) * (maxPointCount - 1), Allocator.Temp);
+            for (int s = 0; s < (maxPointCount - 1); s++)
                 for (int p = 0; p < profile.count - 1; p++)
                 {
                     indices[(s * (profile.count - 1) + p) * 6 + 0] = (s + 0) * profile.count + (p + 0);
-                    indices[(s * (profile.count - 1) + p) * 6 + 1] = (s + 1) * profile.count + (p + 1);
-                    indices[(s * (profile.count - 1) + p) * 6 + 2] = (s + 0) * profile.count + (p + 1);
+                    indices[(s * (profile.count - 1) + p) * 6 + 1] = (s + 0) * profile.count + (p + 1);
+                    indices[(s * (profile.count - 1) + p) * 6 + 2] = (s + 1) * profile.count + (p + 1);
                     indices[(s * (profile.count - 1) + p) * 6 + 3] = (s + 1) * profile.count + (p + 1);
-                    indices[(s * (profile.count - 1) + p) * 6 + 4] = (s + 0) * profile.count + (p + 0);
-                    indices[(s * (profile.count - 1) + p) * 6 + 5] = (s + 1) * profile.count + (p + 0);
+                    indices[(s * (profile.count - 1) + p) * 6 + 4] = (s + 1) * profile.count + (p + 0);
+                    indices[(s * (profile.count - 1) + p) * 6 + 5] = (s + 0) * profile.count + (p + 0);
                 }
 
 
--- Assets/Tracks/Scripts/TrackTester.cs
+++ Assets/Tracks/Scripts/TrackTester.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TrackTester : MonoBehaviour {
 
@@ -75,16 +76,21 @@ public class TrackTester : MonoBehaviour {
     public Transform a, b;
     private void Update()
     {
+        if (Keyboard.current.rKey.isPressed)
+            a.position += Vector3.left * Time.deltaTime;
+        if (Keyboard.current.qKey.isPressed)
+            a.position += Vector3.right * Time.deltaTime;
+
         a.GetPositionAndRotation(out Vector3 aPos, out Quaternion aRot);
         b.GetPositionAndRotation(out Vector3 bPos, out Quaternion bRot);
-        //if (aPos != aLastPos || bPos != bLastPos || aRot != aLastRot || bRot != bLastRot) {
+        if (aPos != aLastPos || bPos != bLastPos || aRot != aLastRot || bRot != bLastRot) {
             aLastPos = aPos;
             bLastPos = bPos;
             aLastRot = aRot;
             bLastRot = bRot;
             controller.SetPoints(a.position, a.rotation, b.position, b.rotation);
             controller.QueueRefresh();
-        //}
+        }
 
         
     }
