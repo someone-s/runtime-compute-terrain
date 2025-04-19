59aeef67d5b02656a57d497f1e57004791e8ba6d
wonge1 <e.wong1@lancaster.ac.uk>
2025-04-19 01:15:15 +0100

    refactor spline

--- Assets/Splines/Scripts/SplineTester.cs
+++ Assets/Splines/Scripts/SplineTester.cs
@@ -3,93 +3,13 @@ using UnityEngine.InputSystem;
 
 public class SplineTester : MonoBehaviour {
 
-    private static SplineProfile[] profiles = new SplineProfile[] {
-        new () {
-            extent = 20f,
-            vertical = true,
-            count = 2,
-            points = new TrackPoint[] 
-            { 
-                new TrackPoint() {
-                    position = new Vector3(-0.75f, 0f, 0f),
-                    normal =   new Vector3( 0f,    1f, 0f),
-                    uv =       new Vector2( 0f,    0f),
-                },
-                new TrackPoint() {
-                    position = new Vector3( 0.75f, 0f, 0f),
-                    normal =   new Vector3( 0f,    1f, 0f),
-                    uv =       new Vector2( 0f,    0f),
-                }
-            },
-        },
-
-        new () {
-            extent = 20f,
-            vertical = true,
-            count = 4,
-            points = new TrackPoint[] 
-            {
-                new TrackPoint() {
-                    position = new Vector3(-10f,   -5f,     0f),
-                    normal =   new Vector3(-0.707f, 0.707f, 0f),
-                    uv =       new Vector2( 0f,     0f),
-                },
-                new TrackPoint() {
-                    position = new Vector3(-0.75f, 0f, 0f),
-                    normal =   new Vector3( 0f,    1f, 0f),
-                    uv =       new Vector2( 0f,    0f),
-                },
-                new TrackPoint() {
-                    position = new Vector3( 0.75f, 0f, 0f),
-                    normal =   new Vector3( 0f,    1f, 0f),
-                    uv =       new Vector2( 0f,    0f),
-                },
-                new TrackPoint() {
-                    position = new Vector3( 10f,   -5f,     0f),
-                    normal =   new Vector3( 0.707f, 0.707f, 0f),
-                    uv =       new Vector2( 0f,     0f),
-                }
-            }
-        },
-
-        new () {
-            extent = 20f,
-            vertical = true,
-            count = 4,
-            points = new TrackPoint[] 
-            { 
-                new TrackPoint() {
-                    position = new Vector3(-10f,     5f,     0f),
-                    normal =   new Vector3( 0.707f, -0.707f, 0f),
-                    uv =       new Vector2( 0f,      0f),
-                },
-                new TrackPoint() {
-                    position = new Vector3(-0.75f, 0f, 0f),
-                    normal =   new Vector3( 0f,    1f, 0f),
-                    uv =       new Vector2( 0f,    0f),
-                },
-                new TrackPoint() {
-                    position = new Vector3( 0.75f, 0f, 0f),
-                    normal =   new Vector3( 0f,    1f, 0f),
-                    uv =       new Vector2( 0f,    0f),
-                },
-                new TrackPoint() {
-                    position = new Vector3( 10f,    5f,     0f),
-                    normal =   new Vector3(-0.707f, 0.707f, 0f),
-                    uv =       new Vector2( 0f,     0f),
-                }
-            },
-        },
-    };
-    public int profileIndex;
-
-    SplineController controller;
+    [SerializeField] private SplineProfile profile;
+    private SplineController controller;
 
     private void Start()
     {
         controller = GetComponent<SplineController>();
-        controller.Setup(profiles[profileIndex]);
-        Debug.Log(JsonUtility.ToJson(profiles[profileIndex], prettyPrint:true));
+        controller.Setup(profile);
     }
 
 
